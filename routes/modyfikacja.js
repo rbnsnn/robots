53c3cc989e56f1f2a682ce41f383d517969dcc17
Miłosz Mokrzycki <mmokrzycki1@imgw.pl>
2022-06-10 13:56:06 +0200

    add edit validation

--- routes/modyfikacja.js
+++ routes/modyfikacja.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Form = require('../models/Form')
+const Config = require('../models/Config')
 const mongoose = require('mongoose')
 
 router.get('/', async (req, res) => {
@@ -29,23 +30,34 @@ router.get('/:id', async (req, res) => {
 
 router.get('/:id/:productionTime/:producedParts', async (req, res) => {
     const isCollectionEmpty = await mongoose.connection.db.collection('forms').estimatedDocumentCount()
+    const { productionTimeMax, productionTimeMin, producedPartsMax, producedPartsMin } = await Config.findOne({}).lean()
 
-    const effectivity = (req.params.producedParts / req.params.productionTime).toFixed(4)
 
-    const editData = {
-        id: req.params.id,
-        productionTime: req.params.productionTime,
-        producedParts: req.params.producedParts,
-        effectivity
-    }
-
-    await Form.findOneAndReplace({ id: req.params.id }, editData)
 
     if (isCollectionEmpty) {
-        const data = await Form.find({}).lean()
-        const selectedElement = await Form.find({ id: req.params.id }).lean()
+        const productionTime = Number(req.params.productionTime)
+        const producedParts = Number(req.params.producedParts)
 
-        res.render('layouts/modyfikacja', { layout: 'index', data, selectedElement })
+        if (productionTime > productionTimeMax || productionTime < productionTimeMin || producedParts > producedPartsMax || producedParts < producedPartsMin) {
+            const selectedElement = await Form.find({ id: req.params.id }).lean()
+            const data = await Form.find({}).lean()
+
+            res.render('layouts/modyfikacja', { layout: 'index', data, selectedElement, editError: true })
+        } else {
+            const effectivity = (producedParts / productionTime).toFixed(4)
+            const editData = {
+                productionTime,
+                producedParts,
+                effectivity
+            }
+
+            await Form.findOneAndUpdate({ id: req.params.id }, editData)
+
+            const selectedElement = await Form.find({ id: req.params.id }).lean()
+            const data = await Form.find({}).lean()
+
+            res.render('layouts/modyfikacja', { layout: 'index', data, selectedElement })
+        }
     } else {
         res.render('layouts/modyfikacja', { layout: 'index', emptyRecords: true })
     }
